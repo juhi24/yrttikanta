7720fa5879bf1444a4e731dd873be689f87c1860
Jussi Tiira <jussi@j24.fi>
2018-02-10 00:57:43 +0200

    dont use realpath

--- yrttikanta/__init__.py
+++ yrttikanta/__init__.py
@@ -2,7 +2,7 @@
 from os import path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-db_path = path.realpath(path.join(path.dirname(__file__), '..', 'data', 'yrttikanta.db'))
+db_path = path.join(path.dirname(path.dirname(__file__)), 'data', 'yrttikanta.db')
 engine = create_engine('sqlite:///{}'.format(db_path), echo=False)
 print(engine)
 Session = sessionmaker(bind=engine)
