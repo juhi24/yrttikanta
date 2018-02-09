a3557c9b98fb7a41ae9a2cbd21e434920eaa9969
Jussi Tiira <jussi@j24.fi>
2018-02-09 22:31:07 +0200

    Session making to init

--- yrttikanta/__init__.py
+++ yrttikanta/__init__.py
@@ -1,1 +1,8 @@
 # coding: utf-8
+from os import path
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+db_path = path.realpath(path.join(path.dirname(__file__), '..', 'data', 'yrttikanta.db'))
+engine = create_engine('sqlite:///{}'.format(db_path), echo=False)
+print(engine)
+Session = sessionmaker(bind=engine)
--- yrttikanta/tools.py
+++ yrttikanta/tools.py
@@ -1,8 +1,3 @@
 # coding: utf-8
 """"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///../data/yrttikanta.db', echo=False)
-Session = sessionmaker(bind=engine)
-session = Session()
