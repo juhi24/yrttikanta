9f07197ec28b67a4133217b43aa53a69fb86ecbd
Jussi Tiira <jussi@j24.fi>
2018-03-14 23:38:02 +0200

    logic implemented in yrttiharava spider

--- scripts/scr_soup.py
+++ scripts/scr_soup.py
@@ -3,33 +3,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
 
-from bs4 import BeautifulSoup, NavigableString, Tag
 import requests
+
 
 if __name__ == '__main__':
     response = requests.get('http://yrttitarha.fi/kanta/valkosipuli/index.html')
-    soup = BeautifulSoup(response.text, 'lxml')
-    sections = {}
-    for t in ('a', 'b', 'i'):
-        for tag in soup.find_all(t):
-            tag.replace_with_children()
-    for h2 in soup.find_all('h2'):
-        if h2.parent.name == 'p':
-            h2.parent.replace_with_children()
-        p_texts = []
-        for s in h2.next_siblings:
-            if s.name == 'h2':
-                break
-            if hasattr(s, 'descendants'):
-                br = False
-                for d in s.descendants:
-                    if d.name == 'h2':
-                        br = True
-                if br:
-                    break
-            if hasattr(s, 'text'):
-                text = s.text.replace('\n', '')
-                if text and text not in ['Ruokaohjeet']:
-                    p_texts.append(text)
-        sections[h2.text] = p_texts or [h2.next_sibling.replace('\n', '')]
+    sections = parse_texts(response)
 
